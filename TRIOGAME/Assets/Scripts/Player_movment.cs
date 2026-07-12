1133ffac0df4e3bc9a0e28dea0ac67ef37e0a8e8
Pawad-Bott <umnas777@gmail.com>
2026-07-12 14:32:51 +0200

    Added movment and förbättrade det!

--- TRIOGAME/Assets/Scripts/Player_movment.cs
+++ TRIOGAME/Assets/Scripts/Player_movment.cs
@@ -1,26 +1,46 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-public class Player_movment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float Mspeed = 3;
-    public Rigidbody RB;
-    public CharacterController CC;
+    public float maxSpeed = 6f;
+    public float acceleration = 15f;
+    public float deceleration = 20f;
 
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    private Vector3 velocity;
+
 
     void Update()
     {
-        Vector3 speed = new Vector3(0, 0, 0);
-        if (Input.GetKey("s")) speed.z = -1;
-        if (Input.GetKey("w")) speed.z = 1;
-        if (Input.GetKey("a")) speed.x = -1;
-        if (Input.GetKey("d")) speed.x = 1;
+        Vector2 input = Vector2.zero;
+
+        // Keyboard input
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.wKey.isPressed) input.y += 1;
+            if (Keyboard.current.sKey.isPressed) input.y -= 1;
+            if (Keyboard.current.aKey.isPressed) input.x -= 1;
+            if (Keyboard.current.dKey.isPressed) input.x += 1;
+        }
+
+        // Controler input
+        if (Gamepad.current != null)
+        {
+            input += Gamepad.current.leftStick.ReadValue();
+        }
+
+        Vector3 targetVelocity = new Vector3(input.x, 0, input.y);
+
+        // This doesn't make the player move faster diagonally
+        if (targetVelocity.magnitude > 1) targetVelocity.Normalize();
+
+        targetVelocity *= maxSpeed;
+
+        float rate = targetVelocity.magnitude > 0 ? acceleration : deceleration;
+
+        velocity = Vector3.MoveTowards(velocity, targetVelocity, rate * Time.deltaTime);
 
 
-        CC.SimpleMove(speed * Mspeed);
+        controller.SimpleMove(velocity);
     }
 }
