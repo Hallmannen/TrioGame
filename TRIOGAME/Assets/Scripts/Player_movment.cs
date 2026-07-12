17ffee65d4b62c17628517d2e59fc3e234b73d9f
Eli4s06 <slussforselias@gmail.com>
2026-07-12 15:28:28 +0200

    AddedMovment And Changed InputSystems
    RequierRestart

--- TRIOGAME/Assets/Scripts/Player_movment.cs
+++ TRIOGAME/Assets/Scripts/Player_movment.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class Player_movment : MonoBehaviour
 {
+    public float Mspeed = 3;
     public Rigidbody RB;
     public CharacterController CC;
 
@@ -13,6 +14,13 @@ public class Player_movment : MonoBehaviour
 
     void Update()
     {
-        
+        Vector3 speed = new Vector3(0, 0, 0);
+        if (Input.GetKey("s")) speed.z = -1;
+        if (Input.GetKey("w")) speed.z = 1;
+        if (Input.GetKey("a")) speed.x = -1;
+        if (Input.GetKey("d")) speed.x = 1;
+
+
+        CC.SimpleMove(speed * Mspeed);
     }
 }
