9cb271042edc98d2d199a162d7dccde275f1c413
Eli4s06 <slussforselias@gmail.com>
2026-07-12 13:43:44 +0200

    Test
    trying to fix bug

--- TRIOGAME/Assets/Scripts/Generate world.cs
+++ TRIOGAME/Assets/Scripts/Generate world.cs
@@ -8,23 +8,12 @@ public class Generateworld : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD:TRIOGAME/Assets/Scripts/Player_Movment.cs
 
-=======
-        
->>>>>>> 68c4f5779a1f597f2bbcb3d3fc65f4663cc0003c:TRIOGAME/Assets/Scripts/Generate world.cs
     }
 
     void FixedUpdate()
     {
-<<<<<<< HEAD:TRIOGAME/Assets/Scripts/Player_Movment.cs
-        //if ()
-        {
-            
-        }
 
-=======
->>>>>>> 68c4f5779a1f597f2bbcb3d3fc65f4663cc0003c:TRIOGAME/Assets/Scripts/Generate world.cs
         
     }
 }
