b04fc675c5bcd81477748c07b5f5adef1b957b8d
Eli4s06 <slussforselias@gmail.com>
2026-07-12 12:44:50 +0200

    Update Player_Movment.cs

--- TRIOGAME/Assets/Scripts/Player_Movment.cs
+++ TRIOGAME/Assets/Scripts/Player_Movment.cs
@@ -11,6 +11,6 @@ public class Player_Movment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Debug.Log("hejsan");
+       Debug.Log("nejsan hejsan??!");
     }
 }
