13070aa306fd8e0cc88c6f32c932797226713b28
Pawad-Bott <umnas777@gmail.com>
2026-07-25 20:04:00 +0200

    fix

--- TRIOGAME/Assets/Scripts/Player/Player_movment.cs
+++ TRIOGAME/Assets/Scripts/Player/Player_movment.cs
@@ -53,11 +53,13 @@ public class Player_Movement : MonoBehaviour
         if (Gamepad.current != null && PlayingWithControler)
         {
             input += Gamepad.current.leftStick.ReadValue();
+            /*
             if (Gamepad.current.buttonEast.isPressed && !IsSprinting)
             {
                 IsSprinting = true;
                 newmaxSpeed = DashSpeed;
             }
+            */
         }
 
         if (IsSprinting)
